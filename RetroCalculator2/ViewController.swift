c807b2a5058eafb1a9f61f3c5b873a6ed6e647be
rambotion <rambo.you@gmail.com>
2016-02-14 11:19:44 -0600

    Initial Commit

---
+++ RetroCalculator2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RetroCalculator2
+//
+//  Created by rambo on 2/14/16.
+//  Copyright © 2016 rambo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
